8ecf24e2aaac85aa100697e4ed34506d8e4c7405
iabdoou <abdou._.dz@icloud.com>
2026-03-12 20:49:02 -0400

    Delete prisma directory

--- prisma/migrations/20260310010534_tracking/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "TrackingEvent" ADD COLUMN     "containerTrackingCode" TEXT DEFAULT 'EGSU6241911';
--- prisma/migrations/20260310142535_tracking_modif/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "containerTrackingCode" TEXT DEFAULT 'EGSU6241911',
-ADD COLUMN     "lastTrackedTime" TIMESTAMP(3),
-ADD COLUMN     "trackedData" JSONB;
